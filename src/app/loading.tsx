b63f883954153c8f177f6f8c3eac6125b01ae911
onesol98 <ruaenddl981028@naver.com>
2023-03-31 19:21:36 +0900

    [FEAT]: loading component add

---
+++ src/app/loading.tsx
@@ -0,0 +1,10 @@
+import Loader from "../../public/icons/img_loading.svg";
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div className="flex justify-center">
+      <Loader />
+    </div>
+  );
+}
