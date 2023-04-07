1fffb5c9481c69db6d970c9d5389bb851e257123
onesol98 <71314689+hansolbangul@users.noreply.github.com>
2023-04-07 11:21:20 +0900

    [FEAT]: notion theme 분기 처리

---
+++ src/components/Notion/NotionItem/NotionRender.tsx
@@ -0,0 +1,65 @@
+'use client'
+
+import React from 'react'
+import { NotionRenderer } from 'react-notion-x'
+import "react-notion-x/src/styles.css";
+import "prismjs/themes/prism-tomorrow.css";
+import "katex/dist/katex.min.css";
+import dynamic from "next/dynamic";
+import { Code } from "react-notion-x/build/third-party/code";
+import Link from "next/link";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import { NotionExtendedRecordMap } from '@/networks/network';
+import { useTheme } from 'next-themes';
+
+// 'dark' | 'light'
+type ThemeStatus = string | undefined
+
+type Props = {
+  blockMap: NotionExtendedRecordMap;
+};
+
+const Pdf = dynamic(() => import("react-notion-x/build/third-party/pdf").then((m) => m.Pdf), {
+  ssr: false,
+});
+
+const Modal = dynamic(() => import("react-notion-x/build/third-party/modal").then((m) => m.Modal), { ssr: false });
+
+
+const mapPageUrl = (id: string) => {
+  return "https://www.notion.so/" + id.replace(/-/g, "");
+};
+
+export default function NotionRender({blockMap}: Props) {
+  const { systemTheme, theme } = useTheme();
+  
+  const [isDark, setTheme] = useState<ThemeStatus>();
+
+  useEffect(() => {
+    const currentTheme = theme === "system" ? systemTheme : theme;
+    setTheme(currentTheme)
+  }, [theme])
+
+  return (
+    <>
+      {isDark && <NotionRenderer
+        className="dark:bg-zinc-950 bg-white"
+        recordMap={blockMap}
+        mapPageUrl={mapPageUrl}
+        fullPage={true}
+        showCollectionViewDropdown={false}
+        disableHeader
+        components={{
+          nextImage: Image,
+          nextLink: Link,
+          Code,
+          Modal,
+          Pdf,
+        }}
+        darkMode={isDark === 'dark'}
+      />}
+    </>
+    
+  )
+}
--- src/components/Notion/NotionPage.tsx
+++ src/components/Notion/NotionPage.tsx
@@ -1,58 +1,22 @@
 "use client";
-import * as React from "react";
-import { NotionRenderer } from "react-notion-x";
-import "react-notion-x/src/styles.css";
-import "prismjs/themes/prism-tomorrow.css";
-import "katex/dist/katex.min.css";
-import dynamic from "next/dynamic";
-import { Code } from "react-notion-x/build/third-party/code";
-import { useTheme } from "next-themes";
 import { NotionExtendedRecordMap, TPost } from "@/networks/network";
-import Link from "next/link";
-import Image from "next/image";
 import NotionThumbnail from "./NotionItem/Thumbnail";
 import Comment from "../Utteranc/Comment";
-
-const Pdf = dynamic(() => import("react-notion-x/build/third-party/pdf").then((m) => m.Pdf), {
-  ssr: false,
-});
-
-const Modal = dynamic(() => import("react-notion-x/build/third-party/modal").then((m) => m.Modal), { ssr: false });
+import NotionRender from "./NotionItem/NotionRender";
 
 type Props = {
   blockMap: NotionExtendedRecordMap;
   post: TPost;
 };
 
-const mapPageUrl = (id: string) => {
-  return "https://www.notion.so/" + id.replace(/-/g, "");
-};
-
 export default function NotionPage({ blockMap, post }: Props) {
-  const { systemTheme, theme } = useTheme();
-  const currentTheme = theme === "system" ? systemTheme : theme;
 
   return (
     <>
       {blockMap && (
         <div className="-mt-4">
           {post.thumbnail && <NotionThumbnail thumbnail={post.thumbnail} />}
-          <NotionRenderer
-            className="dark:bg-zinc-950 bg-white"
-            recordMap={blockMap}
-            mapPageUrl={mapPageUrl}
-            fullPage={true}
-            showCollectionViewDropdown={false}
-            disableHeader
-            components={{
-              nextImage: Image,
-              nextLink: Link,
-              Code,
-              Modal,
-              Pdf,
-            }}
-            darkMode={currentTheme === "dark"}
-          />
+          <NotionRender blockMap={blockMap}/>
           {post.type[0] === "Post" && (
             <>
               <Comment post={post} />
