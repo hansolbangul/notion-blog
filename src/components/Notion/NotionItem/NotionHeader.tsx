0156d1bf3a0c5e5b097b03aeccb36b5dd683d169
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2023-04-07 22:43:14 +0900

    [FIX]: bug fix

--- src/components/Notion/NotionItem/NotionHeader.tsx
+++ src/components/Notion/NotionItem/NotionHeader.tsx
@@ -1,41 +1,42 @@
-'use client'
+"use client";
 
-import TagIcon from '@/components/Post/PostItem/TagIcon'
-import { TPost } from '@/networks/network'
-import { useRouter } from 'next/navigation';
-import React from 'react'
-import {IoShareSocialSharp} from "react-icons/io5";
-
+import TagIcon from "@/components/Post/PostItem/TagIcon";
+import { TPost } from "@/networks/network";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { IoShareSocialSharp } from "react-icons/io5";
 
 type Props = {
   post: TPost;
-  isDark: boolean
-}
+};
 
-export default function NotionHeader({post, isDark}: Props) {
-  
+export default function NotionHeader({ post }: Props) {
   return (
-    <div className='flex flex-col py-7 border-y'>
-      <h1 className='font-bold text-transparent tracking-tight max-w-2xl text-5xl'>
-          <span className='inline-block bg-clip-text bg-gradient-to-tr to-indigo-300 from-indigo-600'>
-          {post.title}
-        </span>
+    <div className="flex flex-col py-7 border-y">
+      <h1 className="font-bold text-transparent tracking-tight max-w-2xl text-5xl">
+        <span className="inline-block bg-clip-text bg-gradient-to-tr to-indigo-300 from-indigo-600">{post.title}</span>
       </h1>
-      <div className='pt-6 flex space-x-4 text-base items-center'>
-        <div className='flex flex-col space-y-1'>
-          <span className='font-normal'>생성일</span>
-          <span className='font-bold'>{new Intl.DateTimeFormat("ko").format(new Date(post.date.start_date))}</span>
+      <div className="pt-6 flex space-x-4 text-base items-center">
+        <div className="flex flex-col space-y-1">
+          <span className="font-normal">생성일</span>
+          <span className="font-bold">{new Intl.DateTimeFormat("ko").format(new Date(post.date.start_date))}</span>
         </div>
-        <div className='flex flex-col space-y-1'>
-          <span className='font-normal'>태그</span>
-          <span className='flex space-x-2'>{post.tags?.slice(0,2).map((tag) => <TagIcon key={tag} tag={tag}/>)}</span>
+        <div className="flex flex-col space-y-1">
+          <span className="font-normal">태그</span>
+          <span className="flex space-x-2">
+            {post.tags?.slice(0, 2).map((tag) => (
+              <TagIcon key={tag} tag={tag} />
+            ))}
+          </span>
         </div>
-        <div className='flex flex-col space-y-1'>
-          <span className='font-normal'>작성자</span>
-          <span className='font-bold'>{post.author ? post.author[0].name : '미등록'}</span>
+        <div className="flex flex-col space-y-1">
+          <span className="font-normal">작성자</span>
+          <span className="font-bold">{post.author ? post.author[0].name : "미등록"}</span>
         </div>
-        <div className='flex flex-auto justify-en cursor-pointer'><IoShareSocialSharp/></div>
+        <div className="flex flex-auto justify-en cursor-pointer">
+          <IoShareSocialSharp />
+        </div>
       </div>
     </div>
-  )
+  );
 }
