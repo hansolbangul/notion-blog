ffb249f701fe3ee02bf7b7e51b60fea5b93a685b
onesol98 <71314689+hansolbangul@users.noreply.github.com>
2023-04-12 22:17:01 +0900

    [FEAT]: error.tsx

---
+++ src/app/error.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import React from 'react'
+
+type Props = {
+  errorType?: 'NOT_FOUND' | 'UNKNOWN'
+}
+
+
+export default function error() {
+  return (
+    <div
+      className={`m-auto max-w-4xl bg-white dark:bg-zinc-700 rounded-3xl py-12 px-6 shadow-md`}
+    >
+      <div className="py-20 flex flex-col items-center gap-10">
+        <div className="text-6xl flex  items-center">
+          <div>4</div>
+          <div>0</div>
+          <div>4</div>
+        </div>
+        <div className="text-3xl text-gray-500">Post not found</div>
+      </div>
+    </div>
+  )
+}
--- src/components/Header/Items/Title.tsx
+++ src/components/Header/Items/Title.tsx
@@ -4,7 +4,7 @@ import { CONFIG } from "../../../../site.config";
 
 export default function Title() {
   return (
-    <ALink cache={false} className="font-bold flex" href={"/"}>
+    <ALink cache={false} className="font-bold flex cursor-pointer" href={"/"}>
       <div>{CONFIG.blog.title}</div>
     </ALink>
   );
