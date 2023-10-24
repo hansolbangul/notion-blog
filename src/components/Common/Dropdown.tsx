b422dc2beaaf5e8ea2f4a389174b7a2e0ae0b0d6
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2023-10-24 12:09:17 +0900

    [FEAT] headless menu add

---
+++ src/components/Common/Dropdown.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { Menu, Transition } from "@headlessui/react";
+import { Fragment, useEffect, useRef, useState } from "react";
+import { Bars4Icon } from "@heroicons/react/20/solid";
+import useQuery from "@/src/hook/useQuery";
+
+type Props = {
+  tags: {
+    [index: string]: number;
+  };
+};
+
+const Dropdown = ({ tags }: Props) => {
+  const params = useQuery();
+  const selectTag = params.get("tag") || "All";
+
+  const setTag = (tag: string) => {
+    if (selectTag === tag) {
+      params.set("tag", "");
+    } else {
+      params.set("tag", tag);
+    }
+  };
+
+  return (
+    <div className="">
+      <Menu as="div" className="relative inline-block text-left">
+        <div>
+          <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+            <Bars4Icon className="h-5 w-5 text-black hover:text-violet-100" aria-hidden="true" />
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 mt-2 h-80 overflow-y-scroll scrollbar-hide min-w-[180px] origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="px-1 py-1 ">
+              {Object.keys(tags).map((key) => (
+                <Menu.Item key={key}>
+                  {({ active }) => (
+                    <button
+                      onClick={() => setTag(key)}
+                      className={`${
+                        active ? "bg-violet-500 text-white" : "text-gray-900"
+                      } group flex w-full items-center rounded-md px-2 py-2 text-sm justify-between`}
+                    >
+                      <span>{key}</span>
+                      <span>({tags[key]})</span>
+                    </button>
+                  )}
+                </Menu.Item>
+              ))}
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </div>
+  );
+};
+
+export default Dropdown;
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,36 @@
 import React from "react";
 import Container from "../Elements/Container";
 import { HeaderItem } from "./Items";
-import Menus from "./Items/Menus";
+import Dropdown from "../Common/Dropdown";
+import { getPosts } from "@/src/libs/apis";
+import { DEFAULT_CATEGORY } from "@/src/constants";
+import { filterPosts, getAllSelectItemsFromPosts } from "@/src/libs/utils/notion";
 
-export default function Header() {
+async function getFetch() {
+  try {
+    const posts = await getPosts();
+    const filteredPost = filterPosts(posts);
+    const tags = getAllSelectItemsFromPosts("tags", filteredPost);
+
+    return {
+      tags: {
+        ...tags,
+      },
+    };
+  } catch (error) {
+    throw error;
+  }
+}
+
+export default async function Header() {
+  const { tags } = await getFetch();
+
   return (
     <Container.Flex className="sticky-header justify-center items-center mb-5 py-5 px-4 bg-opacity-60">
       <div className="flex max-w-3xl items-center m-auto w-full px-2">
         <HeaderItem.Title />
         <div className="flex space-x-3 items-center flex-auto justify-end">
-          {/*<Menus />*/}
+          <Dropdown tags={tags} />
         </div>
       </div>
     </Container.Flex>
