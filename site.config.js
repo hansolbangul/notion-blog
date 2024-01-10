2c08d862c9d2836140bd0b70ee45dde7cacd6a58
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2024-01-11 00:59:40 +0900

    [FEAT]: seo 설정 추가

--- site.config.js
+++ site.config.js
@@ -6,11 +6,10 @@ const CONFIG = {
   },
   metadata: {
     title: "데굴데굴 포스팅 | hansolbangul",
-    description: "",
+    description: "개발 로그, 정보를 공유하는 것을 좋아해서 블로그를 작성하는 Frontend Developer 지한솔입니다.",
   },
   blog: {
     title: "데굴데굴 블로그",
-    description: "한솔방울의 돌아버린 프론트엔드 개발 블로그~",
     todayWords: ["hello world", "hansolbangul", "V-log"],
     calendarTitle: "이번달 채용 공고",
   },
@@ -27,4 +26,4 @@ const CONFIG = {
   },
 }
 
-module.exports = { CONFIG }
+module.exports = {CONFIG}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,38 @@
 import "./globals.css";
 import Header from "../components/Header/Header";
-import { CONFIG } from "@/site.config";
+import {CONFIG} from "@/site.config";
 
 export const metadata = {
-  title: CONFIG.blog.title,
+    title: CONFIG.blog.title,
+    description: CONFIG.metadata.description,
+    openGraph: {
+        title: CONFIG.blog.title,
+        type: 'website',
+        url: 'https://blog.hansolbangul.com',
+        description: CONFIG.metadata.description,
+        images: [
+            {
+                url: 'https://media.licdn.com/dms/image/D5603AQEKayUKeemZsw/profile-displayphoto-shrink_200_200/0/1703051207462?e=1710374400&v=beta&t=pceNUTihMi7jIhgT7w6lamOda_nygzCuPgKOJAOZDqk' || '',
+                alt: '지한솔방울 썸넬',
+                width: 1200,
+                height: 630
+            }
+        ]
+    }
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <html lang="en">
-      <head>
-        <meta name="google-site-verification" content="Q1Bav1At-K9niA9pM2HWt_E8s4d17CThq0EvCWS6RPk" />
-      </head>
-      <body>
-        <div id="toast-root" className="fixed right-2/4 transform translate-x-2/4 flex flex-col z-30 w-64 space-y-2" />
-        <div id="modal-root" className="fixed z-30" />
-        <Header />
+export default function RootLayout({children}: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+        <head>
+            <meta name="google-site-verification" content="Q1Bav1At-K9niA9pM2HWt_E8s4d17CThq0EvCWS6RPk"/>
+        </head>
+        <body>
+        <div id="toast-root" className="fixed right-2/4 transform translate-x-2/4 flex flex-col z-30 w-64 space-y-2"/>
+        <div id="modal-root" className="fixed z-30"/>
+        <Header/>
         <div className="max-w-3xl mx-auto relative">{children}</div>
-      </body>
-    </html>
-  );
+        </body>
+        </html>
+    );
 }
