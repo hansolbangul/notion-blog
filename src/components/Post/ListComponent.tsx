e11f4d7b27e2774f3b150049bc012a86fb3bf90a
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2024-03-01 20:25:49 +0900

    [FEAT]: author 을 text 로 변경

--- src/components/Post/ListComponent.tsx
+++ src/components/Post/ListComponent.tsx
@@ -1,88 +1,111 @@
 import React from "react";
 import Link from "next/link";
-import { TPost } from "@/src/types";
+import {TPost} from "@/src/types";
 import ImageWithFallback from "@common/Images/ImageWithFallback";
 
 type Props = {
-  post: TPost;
+    post: TPost;
 };
 
 function Thumbnail(props: { thumbnail: string }) {
-  const { thumbnail } = props;
+    const {thumbnail} = props;
 
-  return (
-    <div className="relative rounded-xl aspect-[340/190] min-w-max w-full h-fit overflow-hidden self-center flex-auto sm:flex-1">
-      <ImageWithFallback
-        alt="thumbnail"
-        src={thumbnail}
-        fill
-        style={{ objectFit: "cover" }}
-      />
-    </div>
-  );
+    return (
+        <div
+            className="relative rounded-xl aspect-[340/190] min-w-max w-full h-fit overflow-hidden self-center flex-auto sm:flex-1">
+            <ImageWithFallback
+                alt="thumbnail"
+                src={thumbnail}
+                fill
+                style={{objectFit: "cover"}}
+            />
+        </div>
+    );
 }
 
-function Profile(props: { authorName: string; profile: string; date: string }) {
-  const { authorName, profile, date } = props;
+//
+// function Profile(props: { authorName: string; profile: string; date: string }) {
+//     const {authorName, profile, date} = props;
+//
+//     return (
+//         <div className="w-full flex gap-2 items-center p-4 bg-slate-50 rounded-lg h-[64px]">
+//             <ImageWithFallback
+//                 alt="profile"
+//                 className="rounded-xl object-cover aspect-square"
+//                 src={profile ?? ""}
+//                 width={44}
+//                 height={44}
+//             />
+//             <div className="h-full flex flex-col justify-between">
+//                 <div className="text-sm">{authorName}</div>
+//                 <div className="text-xs">{date}</div>
+//             </div>
+//         </div>
+//     );
+// }
 
-  return (
-    <div className="w-full flex gap-2 items-center p-4 bg-slate-50 rounded-lg h-[64px]">
-      <ImageWithFallback
-        alt="profile"
-        className="rounded-xl object-cover aspect-square"
-        src={profile ?? ""}
-        width={44}
-        height={44}
-      />
-      <div className="h-full flex flex-col justify-between">
-        <div className="text-sm">{authorName}</div>
-        <div className="text-xs">{date}</div>
-      </div>
-    </div>
-  );
+function Profile(props: { authorName: string; date: string }) {
+    const {authorName, date} = props;
+
+    return (
+        <div className="w-full flex gap-2 items-center p-4 bg-slate-50 rounded-lg h-[64px]">
+            {/*<ImageWithFallback*/}
+            {/*    alt="profile"*/}
+            {/*    className="rounded-xl object-cover aspect-square"*/}
+            {/*    src={profile ?? ""}*/}
+            {/*    width={44}*/}
+            {/*    height={44}*/}
+            {/*/>*/}
+            <div className="h-full flex flex-col justify-between">
+                <div className="text-sm">{authorName}</div>
+                <div className="text-xs">{date}</div>
+            </div>
+        </div>
+    );
 }
 
 function Contents(props: { title: string; summary?: string }) {
-  const { title, summary } = props;
+    const {title, summary} = props;
 
-  return (
-    <div className="p-2 w-full flex gap-4 flex-col">
-      <div className="w-full text-base mt-4 truncate">{title}</div>
-      <div className="w-full text-sm truncate">{summary}</div>
-    </div>
-  );
+    return (
+        <div className="p-2 w-full flex gap-4 flex-col">
+            <div className="w-full text-base mt-4 truncate">{title}</div>
+            <div className="w-full text-sm truncate">{summary}</div>
+        </div>
+    );
 }
 
-export default function ListComponent({ post }: Props) {
-  const {
-    title,
-    type: types,
-    thumbnail = "",
-    author: authors,
-    summary,
-    date,
-  } = post;
-  const { start_date } = date;
+export default function ListComponent({post}: Props) {
+    const {
+        title,
+        type: types,
+        thumbnail = "",
+        author: authors,
+        author_new,
+        summary,
+        date,
+    } = post;
+    const {start_date} = date;
 
-  const [author] = authors ?? [];
-  const [type] = types;
-  const href = post.URL || `/${type.toLocaleLowerCase()}/${post.slug}`;
+    // const [author] = authors ?? [];
+    const author = author_new;
+    const [type] = types;
+    const href = post.URL || `/${type.toLocaleLowerCase()}/${post.slug}`;
 
-  return (
-    <Link
-      className="p-2 h-full flex flex-wrap gap-2 bg-white shadow-[0_0_32px_0_rgba(0,0,0,0.07)] rounded-xl overflow-hidden"
-      href={href}
-      target={post.URL ? "_blank" : undefined}
-    >
-      <Thumbnail thumbnail={thumbnail} />
-      <div className="w-full flex-1 flex-shrink flex flex-col justify-between min-w-0">
-        <Contents title={title} summary={summary} />
-        <Profile
-          authorName={author.name}
-          profile={author.profile_photo ?? ""}
-          date={start_date}
-        />
-      </div>
-    </Link>
-  );
+    return (
+        <Link
+            className="p-2 h-full flex flex-wrap gap-2 bg-white shadow-[0_0_32px_0_rgba(0,0,0,0.07)] rounded-xl overflow-hidden"
+            href={href}
+            target={post.URL ? "_blank" : undefined}
+        >
+            <Thumbnail thumbnail={thumbnail}/>
+            <div className="w-full flex-1 flex-shrink flex flex-col justify-between min-w-0">
+                <Contents title={title} summary={summary}/>
+                <Profile
+                    authorName={author}
+                    date={start_date}
+                />
+            </div>
+        </Link>
+    );
 }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,35 +1,37 @@
 export type TPostStatus = "Private" | "Public" | "PublicOnDetail";
 export type TPostType = "Post" | "Paper" | "Page" | "Project";
+
 export interface Author {
-  id: string;
-  name: string;
-  profile_photo?: string;
+    id: string;
+    name: string;
+    profile_photo?: string;
 }
 
 export type TPost = {
-  id: string;
-  date: { start_date: string };
-  type: TPostType[];
-  slug: string;
-  tags?: string[];
-  category?: string[];
-  summary?: string;
-  author?: Author[];
-  title: string;
-  status: TPostStatus[];
-  createdTime: string;
-  fullWidth: boolean;
-  thumbnail?: string;
-  URL?: string;
+    id: string;
+    date: { start_date: string };
+    type: TPostType[];
+    slug: string;
+    tags?: string[];
+    category?: string[];
+    summary?: string;
+    author?: Author[];
+    author_new: string;
+    title: string;
+    status: TPostStatus[];
+    createdTime: string;
+    fullWidth: boolean;
+    thumbnail?: string;
+    URL?: string;
 };
 
 export type TPosts = TPost[];
 
 export type TTags = {
-  [tagName: string]: number;
+    [tagName: string]: number;
 };
 export type TCategories = {
-  [category: string]: number;
+    [category: string]: number;
 };
 
 export type ThemeType = "dark" | "light";
