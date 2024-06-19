fc1449ce1923333d078f6dbfdb8b966aca021314
hansolbangul <71314689+hansolbangul@users.noreply.github.com>
2024-06-19 23:31:48 +0900

    [FEAT]: 구글 애드센스

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,10 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
         <html lang="en">
         <head>
             <meta name="google-site-verification" content="Q1Bav1At-K9niA9pM2HWt_E8s4d17CThq0EvCWS6RPk"/>
+            <script async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2465657218123782"
+                    crossOrigin="anonymous">
+            </script>
         </head>
         <body>
         <div id="toast-root" className="fixed right-2/4 transform translate-x-2/4 flex flex-col z-30 w-64 space-y-2"/>
