ca5157a453ed3e55e6e9913de7b0348efa35295b
hansol-apr <ruaenddl981028@naver.com>
2024-08-27 09:19:28 +0900

    [Feat] metadata 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,28 @@ import "./globals.css";
 import ReactQueryProviders from "@hook/useReactQuery";
 import Header from "@app/(component)/header/Header";
 
+export const metadata = {
+  title: "데굴데굴 블로그",
+  description:
+    "데굴데굴 블로그는 다양한 개발 로고와 프론트엔드 관련 정보를 공유하는 공간입니다.",
+  keywords: "블로그, 개발, 로고, 프론트엔드, 웹 개발, React, JavaScript",
+  openGraph: {
+    title: "데굴데굴 블로그",
+    description:
+      "데굴데굴 블로그는 다양한 개발 로고와 프론트엔드 관련 정보를 공유하는 공간입니다.",
+    url: "https://blog.hansolbangul.com",
+    type: "website",
+    images: [
+      {
+        url: "/main_img.webp",
+        alt: "지한솔방울 썸넬",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -19,6 +41,29 @@ export default function RootLayout({
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2465657218123782"
           crossOrigin="anonymous"
         ></script>
+        <script
+          type="application/ld+json"
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              "@context": "https://schema.org",
+              "@type": "WebSite",
+              name: "데굴데굴 블로그", // 사이트 이름을 한글로 설정
+              url: "https://blog.hansolbangul.com",
+              description:
+                "데굴데굴 블로그는 다양한 개발 로고와 프론트엔드 관련 정보를 공유하는 공간입니다.",
+              publisher: {
+                "@type": "Organization",
+                name: "데굴데굴 블로그",
+              },
+              image: {
+                "@type": "ImageObject",
+                url: "/main_img.webp",
+                width: 1200,
+                height: 630,
+              },
+            }),
+          }}
+        />
       </head>
       <body>
         <div
