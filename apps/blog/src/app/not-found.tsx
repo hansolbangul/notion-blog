5b9e849bd5fae651fc448c9987ba2bfce002376d
hansolbangul <ruaenddl981028@naver.com>
2024-10-14 07:59:21 +0900

    [Fix] 스타일 수정

--- apps/blog/src/app/not-found.tsx
+++ apps/blog/src/app/not-found.tsx
@@ -6,8 +6,8 @@ const ErrorButtonWrap = dynamic(() => import("@app/ErrorButtonWrap"), {
 
 export default function NotFound() {
   return (
-    <div className="flex flex-col min-h-screen p-4 relative">
-      <div className="p-6 max-w-md absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+    <div className="flex flex-col p-4 relative">
+      <div className="p-6 w-full fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
         <ErrorButtonWrap />
       </div>
     </div>
