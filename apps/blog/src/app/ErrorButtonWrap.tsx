a37f9e6ddcc5e91fdd3f85d9cde784f66f808a8b
hansolbangul <ruaenddl981028@naver.com>
2024-10-14 01:13:20 +0900

    Update ErrorButtonWrap.tsx

--- apps/blog/src/app/ErrorButtonWrap.tsx
+++ apps/blog/src/app/ErrorButtonWrap.tsx
@@ -1,23 +1,23 @@
 "use client";
 
+import Link from "next/link";
 import Button from "@blog/ui/components/commons/Button";
-import { useRouter } from "next/router";
 import ErrorLottie from "@blog/ui/components/layouts/ErrorLottie";
 
 export default function ErrorButtonWrap() {
-  const router = useRouter();
-
-  if (!router.isReady) return null;
-
   return (
     <>
       <ErrorLottie />
       <div className="flex space-x-4 mt-6">
-        <Button.Warring onClick={() => router.back()}>뒤로가기</Button.Warring>
+        <Button.Warring
+          onClick={() => typeof window !== undefined && window.history.back()}
+        >
+          뒤로가기
+        </Button.Warring>
 
-        <Button.Primary onClick={() => router.reload()}>
-          새로고침
-        </Button.Primary>
+        <Link href="/">
+          <Button.Primary>새로고침</Button.Primary>
+        </Link>
       </div>
     </>
   );
