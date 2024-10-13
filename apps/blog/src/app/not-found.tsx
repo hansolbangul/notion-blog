48949a78bec3db575e64d1e4608b6e1efad19f26
hansolbangul <ruaenddl981028@naver.com>
2024-10-14 00:21:22 +0900

    Update not-found.tsx

--- apps/blog/src/app/not-found.tsx
+++ apps/blog/src/app/not-found.tsx
@@ -1,6 +1,6 @@
 import ErrorButtonWrap from "@app/ErrorButtonWrap";
 
-export default function NotFound() {
+export default async function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen p-4 bg-gray-100">
       <div className="p-6 max-w-md text-center bg-white shadow-lg rounded-lg">
