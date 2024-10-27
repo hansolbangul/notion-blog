3a5d2491389ee19c3fc0057d68d4ca393d91fc75
hansolbangul <ruaenddl981028@naver.com>
2024-10-27 23:49:36 +0900

    [Fix] fetch 제거

--- apps/blog/src/app/tool/base64/page.tsx
+++ apps/blog/src/app/tool/base64/page.tsx
@@ -1,32 +1,5 @@
-import { getCachedPosts } from "@blog/notions/libs/react-query/getCachePosts";
-import postQueryOptions from "@blog/notions/service/postService";
-import {
-  getDehydratedQueries,
-  Hydrate,
-} from "@blog/notions/libs/react-query/nextQuery";
 import React from "react";
-import LetterCountContent from "@app/tool/letter-count/Content";
-
-async function getFetch() {
-  const posts = await getCachedPosts();
-  const { queryKey } = postQueryOptions.all();
-
-  return {
-    dehydratedState: await getDehydratedQueries([
-      {
-        queryKey,
-        queryFn: () => posts,
-      },
-    ]),
-  };
-}
 
 export default async function Page() {
-  const { dehydratedState } = await getFetch();
-
-  return (
-    <Hydrate state={dehydratedState}>
-      <LetterCountContent />
-    </Hydrate>
-  );
+  return <div />;
 }
