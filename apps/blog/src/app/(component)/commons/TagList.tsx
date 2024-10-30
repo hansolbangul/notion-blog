b2c2636185d61f86771b569c4cced91386f8b582
hansolbangul <ruaenddl981028@naver.com>
2024-10-30 22:05:37 +0900

    [Feat] 태그 추가

--- apps/blog/src/app/(component)/commons/TagList.tsx
+++ apps/blog/src/app/(component)/commons/TagList.tsx
@@ -1,15 +1,27 @@
+"use client";
+
 import Link from "next/link";
 import React from "react";
+import { useSearchParams } from "next/navigation";
+import classNames from "classnames";
 
 interface Props {
   tags: string[];
 }
 export default function TagList({ tags }: Props) {
+  const searchParams = useSearchParams();
+  const currentTag = searchParams.get("tag") || "/";
+
   return (
     <div className={"mt-4 w-full flex gap-2 flex-wrap"}>
       {tags.map((tag) => (
         <Link key={tag} href={`/?tag=${tag}`}>
-          <div key={tag} className={"px-2 py-1 rounded-lg bg-gray-100"}>
+          <div
+            key={tag}
+            className={classNames("px-2 py-1 rounded-lg bg-gray-100", {
+              "bg-gray-300": currentTag === tag,
+            })}
+          >
             <span className={"text-body13 text-gray-400"}>{tag}</span>
           </div>
         </Link>
