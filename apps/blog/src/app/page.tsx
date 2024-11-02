bfcc9dd32e92cc784e57afbb7716f5287ac6bc7e
hansolbangul <ruaenddl981028@naver.com>
2024-11-02 14:57:45 +0900

    Update page.tsx

--- apps/blog/src/app/page.tsx
+++ apps/blog/src/app/page.tsx
@@ -11,8 +11,6 @@ import getCached from "@blog/notions/libs/react-query/getCached";
 import { Metadata } from "next";
 import CONFIG from "@blog/notions/site.config";
 
-export const revalidate = 600;
-
 export const metadata = CONFIG.metadata;
 
 async function getFetch() {
