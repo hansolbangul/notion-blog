b99c9d792f98396b9211d17b5131bc10ae2d120a
hansolbangul <ruaenddl981028@naver.com>
2024-11-02 17:04:24 +0900

    Update page.tsx

--- apps/blog/src/app/tool/qr/page.tsx
+++ apps/blog/src/app/tool/qr/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import QrContent from "@app/tool/qr/Content";
 import CONFIG from "@blog/notions/site.config";
 
+// 매타데이터 테스트 중
 export const metadata = CONFIG.metadata;
 
 export default function Page() {
