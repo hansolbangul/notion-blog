7ca82368dfa6b93df06c7eb27f547e150fd6b847
hansolbangul <ruaenddl981028@naver.com>
2024-11-02 17:07:56 +0900

    Revert "Update page.tsx"
    This reverts commit 150ef97601d3df9fa61d25cadfdf77ff8fa1f45c.

--- apps/blog/src/app/tool/qr/page.tsx
+++ apps/blog/src/app/tool/qr/page.tsx
@@ -1,8 +1,24 @@
 import React from "react";
 import QrContent from "@app/tool/qr/Content";
-import CONFIG from "@blog/notions/site.config";
 
-export const metadata = CONFIG.metadata;
+export const metadata = {
+  title: "QR 코드 생성기",
+  description:
+    "데굴데굴 블로그의 QR 코드 생성 도구로 빠르고 쉽게 QR 코드를 생성해보세요.",
+  openGraph: {
+    title: "QR 코드 생성기",
+    description: "데굴데굴 블로그에서 제공하는 간편한 QR 코드 생성 도구",
+    images: [
+      {
+        url: "https://blog.hansolbangul.com/main_img.webp",
+        alt: "지한솔방울썸넬",
+        width: 1200,
+        height: 630,
+      },
+    ],
+  },
+  keywords: ["데굴데굴 블로그", "QR 코드 생성", "QR 코드"],
+};
 
 export default function Page() {
   return <QrContent />;
