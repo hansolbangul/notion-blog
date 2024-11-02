ecc2463aa9e4911e3cbc4b5605f7a08c97c12b25
hansolbangul <ruaenddl981028@naver.com>
2024-11-02 17:28:55 +0900

    [Fix] Edit Site config

--- apps/blog/src/app/page.tsx
+++ apps/blog/src/app/page.tsx
@@ -8,7 +8,6 @@ import {
 import { getAllSelectItemsFromPosts } from "@blog/notions/utils/notion";
 import React from "react";
 import getCached from "@blog/notions/libs/react-query/getCached";
-import { Metadata } from "next";
 import CONFIG from "@/site.config";
 
 export const revalidate = 600;
