2af9e400787f4b3f3593a2d3ede073e56aa259ec
Ji-Hansol <71314689+hansolbangul@users.noreply.github.com>
2026-03-21 23:59:42 +0900

    Remove main overlay layer

--- packages/ui/components/layouts/CrossView.tsx
+++ packages/ui/components/layouts/CrossView.tsx
@@ -6,7 +6,6 @@ export default function CrossView({ children }: { children: React.ReactNode }) {
     <div className="flex min-h-screen flex-col">
       <Header />
       <main className="relative mx-auto w-full max-w-[1260px] flex-1 px-4 pb-20 pt-6 sm:px-6 lg:px-8">
-        <div className="pointer-events-none absolute inset-y-0 left-4 right-4 bg-[rgba(255,255,255,0.54)] sm:left-6 sm:right-6 lg:left-8 lg:right-8" />
         <div className="relative">{children}</div>
       </main>
       <Footer />
